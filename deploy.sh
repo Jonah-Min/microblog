de4e5e48b35df8686264ea78e45d548927556e47
Jonah-Min <jonahm2472@gmail.com>
2017-10-10 21:07:30 -0400

    Update README and deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ mix deps.get
 mix release.init
 env MIX_ENV=prod mix release --env=prod
 env MIX_ENV=prod mix phx.digest --env=prod --verbose
-env PORT=8000 _build/prod/rel/microblog/bin/microblog foreground
+env PORT=8000 _build/prod/rel/microblog/bin/microblog start
